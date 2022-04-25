21e2219a270fdfdc04c2c891e8e055d7ad70cc4f
Manu Elementary 2022 <realgarcilaso@outlook.com>
2022-04-25 08:26:35 -0500

    17. Drink Water

---
+++ 17 Day Drink Water/script.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const listers = document.getElementById('liters');
+const percetage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
