606834cc2b0dc109d34ae45fc000298f8183c7d2
Manu Elementary 2022 <realgarcilaso@outlook.com>
2022-04-25 09:18:13 -0500

    7.3 Fill in/ fill out
    when you click a cup of full water fills out.

--- 17 Day Drink Water/script.js
+++ 17 Day Drink Water/script.js
@@ -8,6 +8,10 @@ smallCups.forEach((cup, idx) => {
 });
 
 function highlightCups(idx) {
+
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
+        idx--
+    }
     smallCups.forEach((cup, idx2) => {
         if (idx2 <= idx) {
             cup.classList.add('full')
