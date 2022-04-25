91f726016ffe1119a6e5ee19fcf8813627b1c637
Manu Elementary 2022 <realgarcilaso@outlook.com>
2022-04-25 08:37:51 -0500

    17.2 filling cups
    when you click a small cup it fills.

--- 17 Day Drink Water/script.js
+++ 17 Day Drink Water/script.js
@@ -2,3 +2,17 @@ const smallCups = document.querySelectorAll('.cup-small');
 const listers = document.getElementById('liters');
 const percetage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+});
+
+function highlightCups(idx) {
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+}
