df0bb905c71b3cf9d0b9da9f5252e495933aaf00
Manu Elementary 2022 <realgarcilaso@outlook.com>
2022-05-09 11:58:44 -0500

    progress bar

--- 02 Day - Progress Steps/script.js
+++ 02 Day - Progress Steps/script.js
@@ -1,11 +1,39 @@
 const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const circles = document.getElementById('.circle');
+const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
-next.addEventListener('click', ()=>{
-   currentActive++;
+next.addEventListener('click', () => {
+    currentActive++;
 
-    console.log(currentActive);
+    // no pass after number 4
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
 });
+
+prev.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+});
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    });
+
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = (actives.length - 1) / (circles.length -1) * 100 + '%'
+}
